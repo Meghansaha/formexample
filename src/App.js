c07b7b9348d7bae82ac7f3732ca142a8ed8c63e4
giild <peter@giild.com>
2022-04-07 17:38:27 -0400

    it loads data from firebase/firestore now. not including the config settings

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import './App.css';
 import React from 'react';
+import { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom';
 import { useForm } from "react-hook-form"
 import BasicForm from './BasicForm';
 import CuriosityForm from './CuriosityForm';
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore, collection, getDocs } from 'firebase/firestore'
+import { async } from '@firebase/util';
+import { db } from './firebase/FirebaseConfig'
 
 function App() {
+
+const [surveyArray, setSurveyArray ] = useState([]);
+const survCollectionRef = collection(db, "surveys");
+
+useEffect(() => {
+  const getSurveys = async () => {
+    const data = await getDocs(survCollectionRef);
+    //console.log(data);
+    setSurveyArray(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+  }
+
+  getSurveys();
+}, [])
+
   return (
     <>
+    <div>
+      {surveyArray.map((row,index) => {
+        return (
+          <>
+            <div >name {row.name} / author {row.author} </div>
+          </>
+        )
+      })}
+    </div>
     <div className='form-container'>
       <CuriosityForm/>
     </div>
